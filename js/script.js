165acc30507ae95f18935a730a89bf418ccf0116
Amelia-Pencak <amelia.pencak@gmail.com>
2023-11-12 13:26:31 +0100

    homePage ver-1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+     document.querySelectorAll('nav a').forEach(link => {
+         link.addEventListener('click', function(e) {
+             e.preventDefault();
+             var url = e.target.href;
+             fetch(url)
+                 .then(response => response.text())
+                 .then(data => {
+                     document.getElementById('content').innerHTML = data;
+                 })
+                 .catch(error => console.error('Błąd ładowania strony', error));
+         });
+     });
+ });
+ 
+
+const words = ["zgłoszeniami", "raporatmi", "zadaniami", "wnioskami", "problemami"];
+let currentWordIndex = 0;
+
+const dynamicWord = document.getElementById("dynamicWord");
+
+function rotateWord() {
+    dynamicWord.classList.add("rotating");
+
+    setTimeout(() => {
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+        dynamicWord.textContent = words[currentWordIndex];
+
+        dynamicWord.classList.remove("rotating");
+    }, 500);
+}
+
+setInterval(rotateWord, 3000);
+
+
+
