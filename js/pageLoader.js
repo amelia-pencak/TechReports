414f7b784a3e4fcde03a2f148aa451b723228ea0
Amelia-Pencak <amelia.pencak@gmail.com>
2023-11-12 17:41:11 +0100

    script js - page loader

---
+++ js/pageLoader.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+     document.querySelectorAll('nav a').forEach(link => {
+         link.addEventListener('click', function(e) {
+             e.preventDefault();
+             var url = e.target.href;
+             fetch(url)
+                 .then(response => response.text())
+                 .then(data => {
+                     document.getElementById('content').innerHTML = data;
+                 })
+                 .catch(error => console.error('Błąd ładowania strony', error));
+         });
+     });
+ });
+ 
+
+
