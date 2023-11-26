4f7f563d7fa217bb4a35000a807138226c8dd56a
Amelia-Pencak <amelia.pencak@gmail.com>
2023-11-26 15:53:43 +0100

    page loader working correctly

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-     document.querySelectorAll('nav a').forEach(link => {
-         link.addEventListener('click', function(e) {
-             e.preventDefault();
-             var url = e.target.href;
-             fetch(url)
-                 .then(response => response.text())
-                 .then(data => {
-                     document.getElementById('content').innerHTML = data;
-                     initializeSlider();
-                 })
-                 .catch(error => console.error('Błąd ładowania strony', error));
-         });
-     });
- });
-
-
- 
+ document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            if (e.target.href.endsWith('index.html') ) {
+                return;
+            }
+            e.preventDefault();
+            var url = e.target.href;
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('content').innerHTML = data;
+                    initializeSlider();
+                })
+                .catch(error => console.error('Błąd ładowania strony', error));
+        });
+    });
+});
